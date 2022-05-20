106cf80fe3e6256706c75338928aeab0bd79507f
peyow20 <69825930+peyow20@users.noreply.github.com>
2022-05-20 16:36:44 -0500

    Update Consumer.java

--- ProducerConsumer/src/producerconsumer/Consumer.java
+++ ProducerConsumer/src/producerconsumer/Consumer.java
@@ -1,28 +1,41 @@
-
 package producerconsumer;
+
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.HashMap;
+
 
 public class Consumer extends Thread {
     Buffer buffer;
+    int numeroConsumidores, sleepConsumidores, id;
+    HashMap <String,String> tareasPorHacer;
+    HashMap <String,String> tareasRealizadas;
     
-    Consumer(Buffer buffer) {
+    
+    Consumer(Buffer buffer, int numeroConsumidores, int sleepConsumidores, HashMap <String,String> tareasPorHacer,HashMap <String,String> tareasRealizadas, int id ) {
         this.buffer = buffer;
+        this.numeroConsumidores = numeroConsumidores;
+        this.sleepConsumidores = sleepConsumidores;
+        this.tareasPorHacer = tareasPorHacer;
+        this.tareasRealizadas = tareasRealizadas;
+        this.id = id;
     }
-    
+ 
+    public void st(){
+    this.stop();
+    }
     @Override
     public void run() {
         System.out.println("Running Consumer...");
-        char product;
+        String product;
         
-        for(int i=0 ; i<5 ; i++) {
+        for(int i=0 ;; i++) {
             product = this.buffer.consume();
-            //System.out.println("Consumer consumed: " + product);
-            Buffer.print("Consumer consumed: " + product);
+            System.out.println("Consumer consumed: " + product);
             
             try {
-                Thread.sleep(1000);
+                Thread.sleep(sleepConsumidores);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
             }
